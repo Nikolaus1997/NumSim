cf47e0d5ff7de9d93f3e5c7b17c0c4f2a6962d42
IAGVOHWI <nikolaus-vohwinkel@web.de>
2023-12-05 20:20:22 +0100

    Partitioning

--- src/partitioning/partitioning.cpp
+++ src/partitioning/partitioning.cpp
@@ -12,31 +12,151 @@ Partitioning::Partitioning(std::array<int, 2> nCellsGlobal)
 
     MPI_Dims_create(nProcs_, 2, Decomposition_.data());
 
+    //Berechnung der lokalen Zellenanzahl in interger wird abgerundet
     nCellsLocal_[0] = nCellsGlobal_[0] / Decomposition_[0];
     nCellsLocal_[1] = nCellsGlobal_[1] / Decomposition_[1];
 
+    //Berechnung der Position der Subdomain
+    nodeposition_[0] = getColumnIndex(rankID_);
+    nodeposition_[1] = getRowIndex(rankID_);   
+    
+    //Berechnung des Rests der Zellenanzahl
     int remainderCol = nCellsGlobal_[0] % Decomposition_[0];
     int remainderRow = nCellsGlobal_[1] % Decomposition_[1];
 
+    //Berechnung der globalen Startzelle der Subdomain als faktor
     int columnDecomposition   = rankID_ % Decomposition_[0] ;
     int rowDecomposistion     = floor(rankID_ / Decomposition_[0]) ;
 
     //Berechnung der globalen Startzelle
     int columnStart = columnDecomposition * nCellsLocal_[0];
-    columnStart += std::min(columnDecomposition, remainderCol);
+    for(int i = 1; i<nodeposition_[0];i++)
+    {
+        if(i< remainderCol) {
+        columnStart++;
+    }
+    }
 
     //Berechnung der globalen Startzelle
     int rowStart = rowDecomposistion * nCellsLocal_[1];
-    rowStart += std::min(rowDecomposistion, remainderRow);
-    
-    //erweitern der lokalen Zellenanzahl um 1 falls nicht perfekte Aufteilung
-    if(columnDecomposition < remainderCol) {
+    for(int i = 1; i<nodeposition_[0];i++)
+    {
+        if(i< remainderRow) {
+            rowStart++;
+        }
+    }
+    //erweitern der lokalen Zellenanzahl um 1 falls nicht perfekte Aufteilung für die domains die am Anfang sind
+    if(nodeposition_[0] <= remainderCol) {
         nCellsLocal_[0] += 1;
     }
-    //erweitern der lokalen Zellenanzahl um 1 falls nicht perfekte Aufteilung
-    if(rowDecomposistion < remainderRow) {
+    //erweitern der lokalen Zellenanzahl um 1 falls nicht perfekte Aufteilung für die domains die am Anfang sind
+    if(nodeposition_[1] <= remainderRow) {
         nCellsLocal_[1] += 1;
     }
 
+    //setzen der Nachbarn
+    if(checkLeftBoundary()) {
+        LeftNeighborRankID_ = rankID_;
+    } else {
+        LeftNeighborRankID_ = calcRankID(nodeposition_[0]-1, nodeposition_[1]);
+    }
+    if(checkRightBoundary()) {
+        RightNeighborRankID_ = rankID_;
+    } else {
+        RightNeighborRankID_ = calcRankID(nodeposition_[0]+1, nodeposition_[1]);
+    }
+    if(checkBottomBoundary()) {
+        BottomNeighborRankID_ = rankID_;
+    } else {
+        BottomNeighborRankID_ = calcRankID(nodeposition_[0], nodeposition_[1]-1);
+    }
+    if(checkTopBoundary()) {
+        TopNeighborRankID_ = rankID_;
+    } else {
+        TopNeighborRankID_ = calcRankID(nodeposition_[0], nodeposition_[1]+1);
+    }
 
+    nodestart_ = {columnStart, rowStart};
 };
+
+int Partitioning::getRankID() {
+    return rankID_;
+}
+
+int Partitioning::getNProcs() {
+    return nProcs_;
+}
+
+std::array<int, 2> Partitioning::getDecomposition() const {
+    return Decomposition_;
+}   
+
+std::array<int, 2> Partitioning::getNCellsGlobal() const{
+    return nCellsGlobal_;
+}
+
+std::array<int, 2> Partitioning::getNCellsLocal() const{
+    return nCellsLocal_;
+}
+
+int Partitioning::getColumnIndex(int rankID) const{
+    return calcColumn(rankID)+1;
+}
+
+int Partitioning::calcColumn(int rankID) const {
+    return rankID % Decomposition_[0];
+}
+
+int Partitioning::calcRow(int rankID) const {
+    return floor(rankID / Decomposition_[0]);
+}
+
+int Partitioning::getRowIndex(int rankID) const{
+    return calcRow(rankID)+1;
+}
+
+std::array<int, 2> Partitioning::getNodeIndeces() const{
+    return nodestart_;
+}
+
+int Partitioning::getDecompositionColumnOrigin() const{
+    return 1;
+}
+
+int Partitioning::getDecompositionRowOrigin()    const{
+    return 1;
+}
+
+int Partitioning::getDecompositionColumnEnd()    const{
+    return Decomposition_[0];
+}
+
+int Partitioning::getDecompositionRowEnd()       const{
+    return Decomposition_[1];
+}
+
+bool Partitioning::checkLeftBoundary()const{
+    return  nodeposition_[0]== getDecompositionColumnOrigin();
+}
+
+bool Partitioning::checkRightBoundary() const{
+    return  nodeposition_[0]== getDecompositionColumnEnd();
+}
+
+bool Partitioning::checkTopBoundary() const{
+    return  nodeposition_[1]== getDecompositionRowOrigin();
+}
+
+bool Partitioning::checkBottomBoundary() const{
+    return  nodeposition_[1]== getDecompositionRowEnd();
+}
+
+//nehme immer den Block in x richtung (Decomposition_[0]) und multipliziere ihn mit der anzahl von reihen
+//addiere die Anzahl von Subdomain spalten die noch übgrig bleiben um bei der subdomain anzukommen 
+//input sind hier die globalen subdomain indeces
+int Partitioning::calcRankID(int column, int row) const{
+    return (column-1) + (row-1) * Decomposition_[0];
+}
+
+
+
--- src/partitioning/partitioning.h
+++ src/partitioning/partitioning.h
@@ -7,13 +7,42 @@
 class Partitioning {
     public:
         Partitioning(std::array<int, 2> nCellsGlobal_);
+        int getRankID();
+        int getNProcs();
+        std::array<int, 2> getDecomposition()   const;
+        std::array<int, 2> getNCellsGlobal()    const;
+        std::array<int, 2> getNCellsLocal()     const;
+        int calcColumn      (int rankID)        const;
+        int getColumnIndex  (int rankID)        const;
+        int calcRow         (int rankID)        const;
+        int getRowIndex     (int rankID)        const;
+        std::array<int, 2> getNodeIndeces()     const;
+        int getDecompositionColumnOrigin()      const;                     
+        int getDecompositionRowOrigin()         const;   
+        int getDecompositionColumnEnd()         const;
+        int getDecompositionRowEnd()            const;   
+        bool checkLeftBoundary()                const;
+        bool checkRightBoundary()               const;
+        bool checkTopBoundary()                 const;
+        bool checkBottomBoundary()              const;
+        int calcRankID(int row, int column)     const;
+        int getLeftAdjacentRankID()             const;
+        int getRightAdjacentRankID()            const;
+        int getTopAdjacentRankID()              const;
+        int getBottomAdjacentRankID()           const;
+
+
     private:
         int nProcs_;
         int rankID_;
         std::array<int, 2> Decomposition_;
         std::array<int, 2> nCellsGlobal_;
         std::array<int, 2> nCellsLocal_;
-
-
+        std::array<int, 2> nodestart_;
+        std::array<int, 2> nodeposition_;
+        int LeftNeighborRankID_;
+        int RightNeighborRankID_;
+        int TopNeighborRankID_;
+        int BottomNeighborRankID_;
 
 };
